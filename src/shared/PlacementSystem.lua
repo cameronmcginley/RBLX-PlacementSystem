a19e0d61e163725b99766342064b3c36ef16d4a5
Cameron McGinley <49362691+cameronmcginley@users.noreply.github.com>
2021-09-17 22:42:10 -0500

    Added normal vector check so we only place on top of base

--- src/shared/PlacementSystem.lua
+++ src/shared/PlacementSystem.lua
@@ -54,7 +54,7 @@ function PlacementSystem:getMousePoint(X, Y)
 		math.round(rawPosition.Y), 
 		math.round(rawPosition.Z))
 
-	return raycastResult.Instance, roundedPosition
+	return raycastResult.Instance, roundedPosition, raycastResult.Normal
 end
 
 function PlacementSystem:Init()
@@ -94,10 +94,18 @@ end
 
 function PlacementSystem:PlacePosition(toPlace, placeEvent)
 	local mouseLocation = UIS:GetMouseLocation()
-	local target, position = self:getMousePoint(mouseLocation.X, mouseLocation.Y)
+	local target, position, normal = self:getMousePoint(mouseLocation.X, mouseLocation.Y)
 
 	if self.placeable.Parent ~= self.Tycoon.Model then
 		self.placeable.Parent = self.Tycoon.Model
+	end
+
+	-- Only placeable on top face of base
+	if normal then
+		local face = PlacementSystem:NormalToFace(normal, self.Tycoon.Model.Base)
+		if face ~= Enum.NormalId.Top then return end
+	else
+		return
 	end
 
 	-- Move ghost to client target
@@ -116,6 +124,53 @@ function PlacementSystem:PlacePosition(toPlace, placeEvent)
 			return false
 		end
 	end
+end
+
+--[[**
+	https://devforum.roblox.com/t/how-do-you-find-the-side-of-a-part-using-raycasting/655452
+   This function returns the face that we hit on the given part based on
+   an input normal. If the normal vector is not within a certain tolerance of
+   any face normal on the part, we return nil.
+
+    @param normalVector (Vector3) The normal vector we are comparing to the normals of the faces of the given part.
+    @param part (BasePart) The part in question.
+
+    @return (Enum.NormalId) The face we hit.
+**--]]
+function PlacementSystem:NormalToFace(normalVector, part)
+
+    local TOLERANCE_VALUE = 1 - 0.001
+    local allFaceNormalIds = {
+        Enum.NormalId.Front,
+        Enum.NormalId.Back,
+        Enum.NormalId.Bottom,
+        Enum.NormalId.Top,
+        Enum.NormalId.Left,
+        Enum.NormalId.Right
+    }    
+
+    for _, normalId in pairs( allFaceNormalIds ) do
+        -- If the two vectors are almost parallel,
+        if self:GetNormalFromFace(part, normalId):Dot(normalVector) > TOLERANCE_VALUE then
+            return normalId -- We found it!
+        end
+    end
+    
+    return nil -- None found within tolerance.
+
+end
+
+--[[**
+    This function returns a vector representing the normal for the given
+    face of the given part.
+
+    @param part (BasePart) The part for which to find the normal of the given face.
+    @param normalId (Enum.NormalId) The face to find the normal of.
+
+    @returns (Vector3) The normal for the given face.
+**--]]
+function PlacementSystem:GetNormalFromFace(part, normalId)
+    return part.CFrame:VectorToWorldSpace(Vector3.FromNormalId(normalId))
 end
 
 function PlacementSystem:TextureBase()
